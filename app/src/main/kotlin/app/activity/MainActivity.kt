6b53bf1a37411ac9389b3e4968c8b6f81f242397
Levi Bostian <levi@levi.earth>
2025-09-05 07:06:44 -0500

    feat: enable edge-to-edge android

--- app/src/main/kotlin/app/activity/MainActivity.kt
+++ app/src/main/kotlin/app/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import app.DiGraph
 import app.android.workManager
 import app.diGraph
@@ -25,6 +26,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             AppTheme {
